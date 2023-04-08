e410b4f0060f704c915d65344e8f1617df75fe47
EC2 Default User <ec2-user@ip-172-31-8-37.ap-northeast-1.compute.internal>
2023-04-08 01:40:55 +0000

    [Add]create,updateが成功した時のフラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,8 +10,13 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to book_path(book.id)
+    else
+      flash[:alert] = '入力してください。'
+      render :index
+    end
   end
 
   def edit
@@ -21,6 +26,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] = 'Book was successfully updated.'
     redirect_to book_path(book.id)
   end
 
