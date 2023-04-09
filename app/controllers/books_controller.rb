ec49d938a6f8515bfad32f7106498bef118e1efa
EC2 Default User <ec2-user@ip-172-31-8-37.ap-northeast-1.compute.internal>
2023-04-09 06:27:12 +0000

    [Add]バリデーション実装（updateが失敗した時）

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,15 +24,21 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = 'Book was successfully updated.'
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    if @book.save
+      flash[:notice] = 'Book was successfully updated.'
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = 'Book was successfully destroyed.'
     redirect_to '/books'
   end
 
